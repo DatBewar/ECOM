e0991b887ab1bb2c8cb8d9b7f81698774ab95ba4
malick DIAGNE <milkzodiagne@yahoo.fr>
2015-11-11 22:05:06 +0100

    base du site

---
+++ ecom/src/main/java/com/groupecom2015/entities/ArticleCommandePK.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupecom2015.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author malick
+ */
+@Embeddable
+public class ArticleCommandePK implements Serializable {
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idArticle")
+    private int idArticle;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idCommande")
+    private int idCommande;
+
+    public ArticleCommandePK() {
+    }
+
+    public ArticleCommandePK(int idArticle, int idCommande) {
+        this.idArticle = idArticle;
+        this.idCommande = idCommande;
+    }
+
+    public int getIdArticle() {
+        return idArticle;
+    }
+
+    public void setIdArticle(int idArticle) {
+        this.idArticle = idArticle;
+    }
+
+    public int getIdCommande() {
+        return idCommande;
+    }
+
+    public void setIdCommande(int idCommande) {
+        this.idCommande = idCommande;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) idArticle;
+        hash += (int) idCommande;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ArticleCommandePK)) {
+            return false;
+        }
+        ArticleCommandePK other = (ArticleCommandePK) object;
+        if (this.idArticle != other.idArticle) {
+            return false;
+        }
+        if (this.idCommande != other.idCommande) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.groupecom2015.entities.ArticleCommandePK[ idArticle=" + idArticle + ", idCommande=" + idCommande + " ]";
+    }
+    
+}
---
+++ ecom/src/main/java/com/groupecom2015/entities/Commentaire.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupecom2015.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author malick
+ */
+@Entity
+@Table(name = "Commentaire")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Commentaire.findAll", query = "SELECT c FROM Commentaire c"),
+    @NamedQuery(name = "Commentaire.findByIdCommentaire", query = "SELECT c FROM Commentaire c WHERE c.idCommentaire = :idCommentaire"),
+    @NamedQuery(name = "Commentaire.findByNoteCommentaire", query = "SELECT c FROM Commentaire c WHERE c.noteCommentaire = :noteCommentaire")})
+public class Commentaire implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idCommentaire")
+    private Integer idCommentaire;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "noteCommentaire")
+    private int noteCommentaire;
+    @JoinColumn(name = "idCompte", referencedColumnName = "idCompte")
+    @ManyToOne(optional = false)
+    private CompteUser idCompte;
+    @JoinColumn(name = "idArticle", referencedColumnName = "idArticle")
+    @OneToOne(optional = false)
+    private Article idArticle;
+
+    public Commentaire() {
+    }
+
+    public Commentaire(Integer idCommentaire) {
+        this.idCommentaire = idCommentaire;
+    }
+
+    public Commentaire(Integer idCommentaire, int noteCommentaire) {
+        this.idCommentaire = idCommentaire;
+        this.noteCommentaire = noteCommentaire;
+    }
+
+    public Integer getIdCommentaire() {
+        return idCommentaire;
+    }
+
+    public void setIdCommentaire(Integer idCommentaire) {
+        this.idCommentaire = idCommentaire;
+    }
+
+    public int getNoteCommentaire() {
+        return noteCommentaire;
+    }
+
+    public void setNoteCommentaire(int noteCommentaire) {
+        this.noteCommentaire = noteCommentaire;
+    }
+
+    public CompteUser getIdCompte() {
+        return idCompte;
+    }
+
+    public void setIdCompte(CompteUser idCompte) {
+        this.idCompte = idCompte;
+    }
+
+    public Article getIdArticle() {
+        return idArticle;
+    }
+
+    public void setIdArticle(Article idArticle) {
+        this.idArticle = idArticle;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idCommentaire != null ? idCommentaire.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Commentaire)) {
+            return false;
+        }
+        Commentaire other = (Commentaire) object;
+        if ((this.idCommentaire == null && other.idCommentaire != null) || (this.idCommentaire != null && !this.idCommentaire.equals(other.idCommentaire))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.groupecom2015.entities.Commentaire[ idCommentaire=" + idCommentaire + " ]";
+    }
+    
+}
