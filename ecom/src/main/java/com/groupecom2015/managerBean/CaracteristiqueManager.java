a8b6b87c0ab9b0fb369ff5b6191718733ca594ca
umar <umar@umar-ubuntu>
2015-11-20 09:31:22 +0100

    delete caracteristique function,(to test) ?

---
+++ ecom/src/main/java/com/groupecom2015/managerBean/CaracteristiqueManager.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupecom2015.managerBean;
+
+import com.groupecom2015.entitieManager.CaracteristiqueFacade;
+import com.groupecom2015.entities.Caracteristique;
+import java.util.List;
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author umar
+ */
+@Named(value = "caracteristiqueManager")
+@ManagedBean
+@RequestScoped
+public class CaracteristiqueManager {
+
+    @EJB
+    private CaracteristiqueFacade caracteristiqueFacade;
+
+    private Caracteristique caracteristique;
+    private List<Caracteristique> caracteristiqueList;
+    /**
+     * Creates a new instance of CaracteristiqueManager
+     */
+    
+    public CaracteristiqueManager() {
+        caracteristique = new Caracteristique();       
+    }
+    
+    public Caracteristique getCaracteristique(){
+        return caracteristique;
+    }
+    
+    public String addCaracteristique(){
+        caracteristiqueFacade.create(caracteristique);
+        return "addCaracteristics";
+    }
+    
+    public List<Caracteristique> getAllCaracteristique(){
+        if(caracteristiqueList == null){
+            caracteristiqueList = caracteristiqueFacade.findAll();
+        }
+        return caracteristiqueList;
+    } 
+    
+    public void delCaracteristique(Caracteristique c){
+        caracteristiqueList = caracteristiqueFacade.findAll();
+        if(caracteristiqueList.contains(c))
+        caracteristiqueFacade.remove(c);
+    }
+    
+    
+}
