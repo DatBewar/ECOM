920240b8119507a7068d6ad40cb831a6d26d66c3
rukundofiston <rukundofiston@yahoo.com>
2015-12-04 10:07:22 +0100

    - Modification quantité en stock d'un article choisi préablement - Afficher Article + commentaires done done

--- ecom/src/main/java/com/groupecom2015/managerBean/ArticleManager.java
+++ ecom/src/main/java/com/groupecom2015/managerBean/ArticleManager.java
@@ -132,4 +132,8 @@ public class ArticleManager {
      public List<Article> getArticleFiltre(){
          return articleFiltre;
      }
+     public String getArticle(int idArticle){
+         article = articleFacade.find(idArticle);
+         return "displayCommentaires";
+     }
 }
