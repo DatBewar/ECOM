fc84988724aded092cf980e2f938d7d702a9c67f
malick DIAGNE <milkzodiagne@yahoo.fr>
2015-12-14 17:24:24 +0100

    problème recherche régler

--- ecom/src/main/java/com/groupecom2015/managerBean/ArticleBackingBean.java
+++ ecom/src/main/java/com/groupecom2015/managerBean/ArticleBackingBean.java
@@ -5,8 +5,12 @@
  */
 package com.groupecom2015.managerBean;
 
+import com.groupecom2015.entitieManager.ArticleFacade;
+import com.groupecom2015.entities.Article;
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -18,12 +22,40 @@ import javax.inject.Named;
 @ManagedBean
 @SessionScoped
 public class ArticleBackingBean implements Serializable {
+    @EJB
+    private ArticleFacade articleFacade;
 
     private int idArticle;
+    private String nomArticle;
+    private Article article;
+    private List<Article> list;
+    public String getNomArticle() {
+        return nomArticle;
+    }
+
+    public List<Article> getList() {
+        return list;
+    }
+
+    public void setList(List<Article> list) {
+        this.list = list;
+    }
+
+    public void setNomArticle(String nomArticle) {
+        this.nomArticle = nomArticle;
+    }
     /**
      * Creates a new instance of ArticleBackingBean
      */
     public ArticleBackingBean() {
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
     }
     
     public int getIdArticle(){
@@ -36,5 +68,15 @@ public class ArticleBackingBean implements Serializable {
     
     public void print(){
         System.out.println(idArticle);
+    }
+    
+    public String getArticleByID(Article article){
+         this.article = article;
+         return "displayCommentaires";
+     }
+    
+     public String getArticlesByKeyWords() {
+        list = articleFacade.findByKeyWords(this.nomArticle);        
+        return "searchResult";
     }
 }
